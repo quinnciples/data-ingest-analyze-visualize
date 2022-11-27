b9823c1350e43b3c31e477ccb7daf1950200f0a3
Paul Simons <alexander.c.quinn@gmail.com>
2022-11-27 21:07:07 +1300

    Added random.normalvariate() usage.

--- sample_data_creation.py
+++ sample_data_creation.py
@@ -10,6 +10,26 @@ def create_sample_data(number_of_items: int = 1) -> List[str]:
     NEW_USERID_RATE: float = 0.25
     INCOMPLETE_ITEM_RATE: float = 0.02
 
+    data_rows: list = []
+    primary_key: int = 0
+    departments: Tuple[str] = ('Marketing', 'Sales', 'Operations')
+    categories: Tuple[str] = ('Hiring Plan', 'Performance Review', 'Compliance Review', 'Budget')
+    steps: Tuple[str] = ('Received', 'Reviewed', 'Handled', 'Finished')
+    steps_branch: Tuple[str] = ('Errors Found', 'Corrected')
+    expected_times: dict[str, tuple] = {
+        # Category: (average, standard deviation)
+        'Hiring Plan': (36.25 / 4.0, 6.29 / 4.0),
+        'Performance Review': (25.0 / 4.0, 5.0 / 4.0),
+        'Compliance Review': (79.65 / 4.0, 4.85 / 4.0),
+        'Budget': (120.0 / 4.0, 16 / 4.0),
+    }
+    # Marketing work takes a little  longer to do, whereas sales requests are quite frequent
+    # and get a lot of attention, therefore people are more comfortable with them and they
+    # require less time. Operations work is the most intensive of the three, and requires
+    # significantly more time.
+    modifiers: dict[str, float] = {'Marketing': 1.05, 'Sales': 0.92, 'Operations': 1.25}
+    padding_length: int = len(str(number_of_items))
+
     def generate_user_id() -> str:
         """
         Helper function for creating fake user id's following a specified pattern.
@@ -35,27 +55,25 @@ def create_sample_data(number_of_items: int = 1) -> List[str]:
             days=days_back, hours=hours_back, minutes=minutes_back, seconds=seconds_back, microseconds=microseconds_back
         )
 
-    def generate_time_increment() -> datetime.timedelta:
+    def generate_time_increment(department: str, category: str, step: str) -> datetime.timedelta:
         """
         Helper function for calculating a random time interval between events.
         """
-        # random.normalvariate(mu=average, sigma=stdev)
+        mu, sigma = expected_times[category]
+        for attribute in [department, category, step]:
+            if attribute in modifiers:
+                mu *= modifiers[attribute]
+                sigma *= modifiers[attribute]
 
-        return datetime.timedelta(
-            days=random.randrange(1, 4) if random.random() < 0.25 else 0,
-            hours=random.randrange(0, 24) if random.random() < 0.5 else 0,
-            minutes=random.randrange(0, 60) if random.random() < 0.25 else 0,
-            seconds=random.randrange(0, 60),
-            microseconds=random.randrange(0, 1_000_000),
-        )
+        return datetime.timedelta(minutes=random.normalvariate(mu=mu, sigma=sigma))
 
-    data_rows: list = []
-    primary_key: int = 1
-    departments: Tuple[str] = ('Marketing', 'Sales', 'Operations')
-    categories: Tuple[str] = ('Hiring_Plan', 'Perf_Review', 'Comp_Review', 'Budget')
-    steps: Tuple[str] = ('Received', 'Reviewed', 'Handled', 'Finished')
-    steps_branch: Tuple[str] = ('Errors_Found', 'Corrected')
-    padding_length: int = len(str(number_of_items))
+        # return datetime.timedelta(
+        #     days=random.randrange(1, 4) if random.random() < 0.25 else 0,
+        #     hours=random.randrange(0, 24) if random.random() < 0.5 else 0,
+        #     minutes=random.randrange(0, 60) if random.random() < 0.25 else 0,
+        #     seconds=random.randrange(0, 60),
+        #     microseconds=random.randrange(0, 1_000_000),
+        # )
 
     for item in range(1, number_of_items + 1):
 
@@ -76,10 +94,9 @@ def create_sample_data(number_of_items: int = 1) -> List[str]:
 
         for step in steps[:-1]:
             # steps[-1] is used because we reserve the final record entry for something special.
-            data_rows.append(f'{primary_key},{item_id},{user_id},{log_time.isoformat()},{department},{category},{step}')
             primary_key += 1
-            time_increment = generate_time_increment()
-            log_time += time_increment
+            log_time += generate_time_increment(department=department, category=category, step=step)
+            data_rows.append(f'{primary_key},{item_id},{user_id},{log_time.isoformat()},{department},{category},{step}')
             if random.random() < ERROR_RATE:
                 # Not everyhing is going to be handled correctly,
                 # so we'll introducte some noise into our data.
@@ -88,12 +105,12 @@ def create_sample_data(number_of_items: int = 1) -> List[str]:
                     # so we'll introducte some noise into our data.
                     user_id = generate_user_id()
                 for branch_step in steps_branch:
+                    primary_key += 1
+                    log_time += generate_time_increment(department=department, category=category, step=step)
                     data_rows.append(
                         f'{primary_key},{item_id},{user_id},{log_time.isoformat()},{department},{category},{branch_step}'
                     )
-                    primary_key += 1
-                    time_increment = generate_time_increment()
-                    log_time += time_increment
+
             if random.random() < NEW_USERID_RATE:
                 # Not everything is going to be started and finished by the same person,
                 # so we'll introducte some noise into our data.
@@ -102,6 +119,8 @@ def create_sample_data(number_of_items: int = 1) -> List[str]:
         if random.random() > INCOMPLETE_ITEM_RATE:
             # Not every item has been completed yet, or maybe the final row of data was lost...
             step = steps[-1]
+            primary_key += 1
+            log_time += generate_time_increment(department=department, category=category, step=step)
             data_rows.append(f'{primary_key},{item_id},{user_id},{log_time.isoformat()},{department},{category},{step}')
             primary_key += 1
 
